e6e9c7d754ca44a2ee36a01ba8f8b84b4c0a5787
Seif Lotfy <seif.lotfy@gmail.com>
2017-06-03 02:25:04 +0200

    more example update

--- README.md
+++ README.md
@@ -20,33 +20,34 @@ unit := time.Hour
 sks := NewSketches(duration, unit, 9, 7, 1.004)
 
 item := []byte("foo")
-t1 := time.Now()
+t1 := time.Date(2017, 06, 03, 0, 0, 0, 0, time.UTC)
 t2 := t1.Add(time.Hour)
-t3 := t2.Add(time.Hour / 6)
+t3 := t2.Add(time.Hour)
+t4 := t3.Add(time.Hour)
 count1 := uint64(1337)
 count2 := uint64(100000)
 
 // Update item for given timestamps
-sks.Insert(item, t2, count1)
+sks.Insert(item, t1, count1)
 sks.Insert(item, t3, count2)
 
 // Estimate count of item within time range [t1, t2]
 got, _ := sks.Estimate(item, t1, t2)
-fmt.Printf("Expected count for \"%s\" in timerange [%v, %v] to be %d, got %d \n",
+fmt.Printf("Expected count for \"%s\" in timerange [%v, %v] to be %d, got %d\n",
     string(item), t1.Format(time.Kitchen), t2.Format(time.Kitchen), count1, got)
 
 // Estimate count of item within time range [t1, t3]
 got, _ = sks.Estimate(item, t1, t3)
-fmt.Printf("Expected count for \"%s\" in timerange [%v, %v] to be %d, got %d \n",
+fmt.Printf("Expected count for \"%s\" in timerange [%v, %v] to be %d, got %d\n",
     string(item), t1.Format(time.Kitchen), t3.Format(time.Kitchen), count1+count2, got)
 
 // Estimate count of item within time range [t1, t3]
-got, _ = sks.Estimate(item, t3, t3.Add(time.Hour/10))
-fmt.Printf("Expected count for \"%s\" in timerange [%v, %v] to be %d, got %d \n",
-    string(item), t1.Format(time.Kitchen), t2.Format(time.Kitchen), count2, got)
+got, _ = sks.Estimate(item, t3, t4)
+fmt.Printf("Expected count for \"%s\" in timerange [%v, %v] to be %d, got %d\n",
+    string(item), t3.Format(time.Kitchen), t4.Format(time.Kitchen), count2, got)
 
 // Output:
-// Expected count for "foo" in timerange [3:21PM, 4:21PM] to be 1337, got 1337
-// Expected count for "foo" in timerange [3:21PM, 4:31PM] to be 101337, got 101337
-// Expected count for "foo" in timerange [3:21PM, 4:21PM] to be 100000, got 100000
+// Expected count for "foo" in timerange [12:00AM, 1:00AM] to be 1337, got 1337
+// Expected count for "foo" in timerange [12:00AM, 2:00AM] to be 101337, got 101337
+// Expected count for "foo" in timerange [2:00AM, 3:00AM] to be 100000, got 100000
 ```
--- sketches_test.go
+++ sketches_test.go
@@ -93,6 +93,52 @@ func TestEstimateReal(t *testing.T) {
 	}
 }
 
+func TestExample(t *testing.T) {
+	duration := time.Duration(720 * time.Hour) // 720 hours range
+	unit := time.Hour
+
+	// Create sketch queryable with
+	// duation = 720 hours range
+	// unit = 1 hour
+	// width per sketch = 2^9
+	// depth per sketch = 8
+	// alpha = 1.004 (used for emphasizing and de-emphasizing)
+	sks := NewSketches(duration, unit, 9, 7, 1.004)
+
+	item := []byte("foo")
+	t1 := time.Date(2017, 06, 03, 0, 0, 0, 0, time.UTC)
+	t2 := t1.Add(time.Hour)
+	t3 := t2.Add(time.Hour)
+	t4 := t3.Add(time.Hour)
+	count1 := uint64(1337)
+	count2 := uint64(100000)
+
+	// Update item for given timestamps
+	sks.Insert(item, t1, count1)
+	sks.Insert(item, t3, count2)
+
+	// Estimate count of item within time range [t1, t2]
+	got, _ := sks.Estimate(item, t1, t2)
+	if count1 != got {
+		t.Errorf("Expected count for \"%s\" in timerange [%v, %v] to be %d, got %d \n",
+			string(item), t1.Format(time.Kitchen), t2.Format(time.Kitchen), count1, got)
+	}
+
+	// Estimate count of item within time range [t1, t3]
+	got, _ = sks.Estimate(item, t1, t3)
+	if count1+count2 != got {
+		t.Errorf("Expected count for \"%s\" in timerange [%v, %v] to be %d, got %d \n",
+			string(item), t1.Format(time.Kitchen), t3.Format(time.Kitchen), count1+count2, got)
+	}
+
+	// Estimate count of item within time range [t1, t3]
+	got, _ = sks.Estimate(item, t3, t4)
+	if count2 != got {
+		t.Errorf("Expected count for \"%s\" in timerange [%v, %v] to be %d, got %d \n",
+			string(item), t3.Format(time.Kitchen), t4.Format(time.Kitchen), count2, got)
+	}
+}
+
 func Example() {
 	duration := time.Duration(720 * time.Hour) // 720 hours range
 	unit := time.Hour
@@ -106,33 +152,34 @@ func Example() {
 	sks := NewSketches(duration, unit, 9, 7, 1.004)
 
 	item := []byte("foo")
-	t1 := time.Now()
+	t1 := time.Date(2017, 06, 03, 0, 0, 0, 0, time.UTC)
 	t2 := t1.Add(time.Hour)
-	t3 := t2.Add(time.Hour / 6)
+	t3 := t2.Add(time.Hour)
+	t4 := t3.Add(time.Hour)
 	count1 := uint64(1337)
 	count2 := uint64(100000)
 
 	// Update item for given timestamps
-	sks.Insert(item, t2, count1)
+	sks.Insert(item, t1, count1)
 	sks.Insert(item, t3, count2)
 
 	// Estimate count of item within time range [t1, t2]
 	got, _ := sks.Estimate(item, t1, t2)
-	fmt.Printf("Expected count for \"%s\" in timerange [%v, %v] to be %d, got %d \n",
+	fmt.Printf("Expected count for \"%s\" in timerange [%v, %v] to be %d, got %d\n",
 		string(item), t1.Format(time.Kitchen), t2.Format(time.Kitchen), count1, got)
 
 	// Estimate count of item within time range [t1, t3]
 	got, _ = sks.Estimate(item, t1, t3)
-	fmt.Printf("Expected count for \"%s\" in timerange [%v, %v] to be %d, got %d \n",
+	fmt.Printf("Expected count for \"%s\" in timerange [%v, %v] to be %d, got %d\n",
 		string(item), t1.Format(time.Kitchen), t3.Format(time.Kitchen), count1+count2, got)
 
 	// Estimate count of item within time range [t1, t3]
-	got, _ = sks.Estimate(item, t3, t3.Add(time.Hour/10))
-	fmt.Printf("Expected count for \"%s\" in timerange [%v, %v] to be %d, got %d \n",
-		string(item), t1.Format(time.Kitchen), t2.Format(time.Kitchen), count2, got)
+	got, _ = sks.Estimate(item, t3, t4)
+	fmt.Printf("Expected count for \"%s\" in timerange [%v, %v] to be %d, got %d\n",
+		string(item), t3.Format(time.Kitchen), t4.Format(time.Kitchen), count2, got)
 
 	// Output:
-	// Expected count for "foo" in timerange [3:21PM, 4:21PM] to be 1337, got 1337
-	// Expected count for "foo" in timerange [3:21PM, 4:31PM] to be 101337, got 101337
-	// Expected count for "foo" in timerange [3:21PM, 4:21PM] to be 100000, got 100000
+	// Expected count for "foo" in timerange [12:00AM, 1:00AM] to be 1337, got 1337
+	// Expected count for "foo" in timerange [12:00AM, 2:00AM] to be 101337, got 101337
+	// Expected count for "foo" in timerange [2:00AM, 3:00AM] to be 100000, got 100000
 }
